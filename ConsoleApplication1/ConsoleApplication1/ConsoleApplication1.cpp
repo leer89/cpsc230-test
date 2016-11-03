87403d9eab0aec956473f53a3c3797d59bd47cdf
leer89 <leer89@gmail.com>
2016-11-03 17:53:31 -0400

    this is some cool stuff

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "hey guise" << endl;
+
+    return 0;
+}
+
